5173e2324d8e568c00e4cc4dd136abec6c6a75ac
Matt Bruce <mbrucedogs@gmail.com>
2021-07-22 17:33:43 -0500

    firebase
    Signed-off-by: Matt Bruce <mbrucedogs@gmail.com>

---
+++ src/services/FirebaseService.js
@@ -0,0 +1,62 @@
+import firebase from '../firebase';
+
+const db = firebase.ref('/controllers');
+
+class FirebaseService {
+  constructor() {
+    this.controllerId = "";
+  }
+
+  getPlayer(controllerId){
+    this.controllerId = controllerId;
+    return this.get('player');
+  }
+
+  getHistory(){
+    return this.get('history');
+  }
+
+  getFavorites(){
+    return this.get('favorites');
+  }
+
+  getNewSongs(){
+    return this.get('newSongs');
+  }
+
+  getSongLists(){
+    return this.get('songLists');
+  }
+
+  getSongs(){
+    return this.get('songs');
+  }
+
+  //generic functions 
+  addPathFor(key){
+    return `${this.controllerId}/${key}`;
+  }
+
+  getPathFor(key){
+    return  key.substr(key.indexOf(this.controllerId));
+  }
+
+  get(key) {
+    return db.child(this.addPathFor(key));
+  }
+
+  add = (key, value) => {
+    db.child(this.getPath(this.pathFor(key))).push(value);
+  };
+
+  update(key, value) {
+    return db.child(this.getPathFor(key)).update(value);
+  }
+
+  delete(key) {
+    return db.child(this.getPathFor(key)).remove();
+  }
+
+}
+
+export default new FirebaseService();

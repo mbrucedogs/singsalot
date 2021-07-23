ea18e04c3d8683006a6c1113f20afe05a0ab14b3
Matt Bruce <mbrucedogs@gmail.com>
2021-07-23 15:46:47 -0500

    moved over firebase calls
    Signed-off-by: Matt Bruce <mbrucedogs@gmail.com>

--- src/services/models.tsx
+++ src/services/models.tsx
@@ -1,4 +1,6 @@
-import { isEmpty } from "lodash";
+import { isEmpty, includes } from "lodash";
+import firebase from 'firebase'
+import FirebaseService from "./FirebaseService";
 
 export interface ISong {
     key?: string | null;
@@ -100,3 +102,113 @@ export const toSongList = (sl: any): ISongList =>{
 
     return songList;
 }
+
+export class FirebaseServiceCache {
+    songLists: ISongList[] = [];
+    singers: ISinger[] = [];
+    queue: IQueueItem[] = [];
+    latestSongs: ISong[] = [];
+    history: ISong[] = [];
+    favorites: ISong[] = [];
+    artists: string[] = [];
+    songs: ISong[] = [];
+
+    constructor() {
+    }
+
+    fetchData(){
+        FirebaseService.getSongLists().on("value", this.onSongListChange);
+        FirebaseService.getPlayerSingers().on("value", this.onSingersChange);
+        FirebaseService.getPlayerQueue().on("value", this.onQueueChange);
+        FirebaseService.getNewSongs().on("value", this.onLatestSongsChange);
+        FirebaseService.getHistory().on("value", this.onHistoryChange);
+        FirebaseService.getFavorites().on("value", this.onFavoritesChange);
+        FirebaseService.getSongLists().on("value", this.onSongsChange);
+    }
+
+    //datachanges
+    onSongListChange(items: firebase.database.DataSnapshot){
+        let list: ISongList[] = [];
+        items.forEach(item => {
+          list.push(toSongList(item.val()));
+        });
+        this.songLists = list;
+    };    
+
+    onSingersChange(items: firebase.database.DataSnapshot) {
+        let list: ISinger[] = [];
+        items.forEach(item => {
+          let obj = item.val();
+          let song = toSinger(obj);
+          song.key = JSON.stringify(item.ref.toJSON())
+          list.push(song);
+        });
+        this.singers = list;
+    };
+    
+    onQueueChange(items: firebase.database.DataSnapshot) {
+        let queueItems: IQueueItem[] = [];
+        items.forEach(item => {
+          let obj = item.val();
+          let newQueueItem: IQueueItem = {
+            key: JSON.stringify(item.ref.toJSON()),
+            singer: toSinger(obj.singer),           
+            song: toSong(obj.song)
+          }
+          queueItems.push(newQueueItem);
+        });
+        this.queue = queueItems;
+    };
+
+    onLatestSongsChange(items: firebase.database.DataSnapshot){
+        let list: ISong[] = [];
+        items.forEach(item => {
+          let obj = item.val();
+          let song = toSong(obj);
+          song.key = JSON.stringify(item.ref.toJSON())
+          list.push(song);
+        });
+        this.latestSongs = list;
+    };
+
+    onHistoryChange(items: firebase.database.DataSnapshot){
+        let list: ISong[] = [];
+        items.forEach(item => {
+          let obj = item.val();
+          let song = toSong(obj);
+          song.key = JSON.stringify(item.ref.toJSON())
+          list.push(song);
+        });
+        this.history = list;
+    };
+    
+    onFavoritesChange(items: firebase.database.DataSnapshot){
+        let list: ISong[] = [];
+        items.forEach(item => {
+          let obj = item.val();
+          let song = toSong(obj);
+          song.key = JSON.stringify(item.ref.toJSON())
+          list.push(song);
+        });
+        this.favorites = list;
+    };
+
+    onSongsChange(items: firebase.database.DataSnapshot){
+        let artists: string[] = [];
+        let list: ISong[] = [];
+
+        items.forEach(item => {
+          let obj = item.val();
+          //get the song
+          list.push(toSong(obj));
+          //get the artist
+          let artist = obj.artist;
+          if(!isEmpty(artist) && !includes(list,artist)){
+            artists.push(artist);
+          }
+        });
+        this.songs = list;
+        this.artists = artists;
+    };
+    
+}  

754c803c3b74946d7216d1d2f36156f71ac10dc3
Matt Bruce <mbrucedogs@gmail.com>
2021-08-02 17:43:05 -0500

    refactored latestSongs
    Signed-off-by: Matt Bruce <mbrucedogs@gmail.com>

---
+++ src/hooks/useLatestSongs.ts
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { ArtistSongs } from "../models/ArtistSongs";
+import { Song } from "../models/Song";
+import { selectLatestArtistSongs, selectLatestSongs } from "../store/store";
+
+export function useLatestSongs(): {
+    latestSongs: Song[];
+    artistSongs: ArtistSongs[]
+} {
+    const latestSongs = useSelector(selectLatestSongs);
+    const artistSongs = useSelector(selectLatestArtistSongs);
+    
+    return { latestSongs , artistSongs }
+}
--- src/pages/LatestSongs.tsx
+++ src/pages/LatestSongs.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { SongPickable } from "../models/SongPickable";
-import { useSelector } from "react-redux";
-import { selectLatestArtistSongs } from "../store/store";
 import { pageCount } from "../globalConfig";
 import SongDiv from "../components/SongDiv";
 import Page from "../components/Page"
 import ScrollingGrid from "../components/ScrollingGrid";
 import { isEmpty } from "lodash";
 import { ArtistSongs } from "../models/ArtistSongs";
+import { useLatestSongs } from "../hooks/useLatestSongs";
 
 const LatestSongs: React.FC<SongPickable> = ({ onSongPick }) => {
-  const listItems:ArtistSongs[] = useSelector(selectLatestArtistSongs);
   const pageName: string = "Latest Songs";
+  const { artistSongs } = useLatestSongs();
   const artistCollapse: boolean = false;
 
-  if (isEmpty(listItems)) {
+  if (isEmpty(artistSongs)) {
     return <Page name={pageName}><h2 style={{ padding: '10px' }}>Loading {pageName}...</h2></Page>
   }
 
@@ -40,7 +39,7 @@ const LatestSongs: React.FC<SongPickable> = ({ onSongPick }) => {
       <ScrollingGrid
         pageCount={pageCount}
         pageName={pageName}
-        listItems={listItems}
+        listItems={artistSongs}
         getRow={(item) => { return buildRow(item)}}
       />
     </Page>

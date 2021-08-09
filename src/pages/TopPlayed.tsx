3a9283cc9c827a2566fcd76a12556b3abda27549
Matt Bruce <mbrucedogs@gmail.com>
2021-08-09 11:13:07 -0500

    formatted

--- src/pages/TopPlayed.tsx
+++ src/pages/TopPlayed.tsx
@@ -21,9 +21,9 @@ const TopSongs: React.FC = () => {
         pageCount={topPlayed.length}
         pageName={pageName}
         listItems={topPlayed}
-        getRow={(history, idx) => {          
+        getRow={(history, idx) => {
           return (<Collapsible key={history.key} trigger={<div className="row-single">
-            <div className="title" style={{ paddingTop: '0px', paddingLeft: '10px', paddingRight: '10px' }}>{ idx! + 1})</div>
+            <div className="title" style={{ paddingTop: '0px', paddingLeft: '10px', paddingRight: '10px' }}>{idx! + 1})</div>
             <div style={{ flex: '1 1 auto' }}>
               <div className="title">({history.count}) {history.artist}</div>
               <div className="subtitle">{history.title}</div>
@@ -31,7 +31,14 @@ const TopSongs: React.FC = () => {
           </div>
           }>
             {history.songs.map(song => {
-              return <SongDiv key={song.key} style={{ paddingLeft: '50px' }} song={song} showCount={true} showPath={true} />
+              return (
+                <SongDiv
+                  key={song.key}
+                  style={{ paddingLeft: '50px' }}
+                  song={song}
+                  showCount={true}
+                  showPath={true} />
+              )
             })}
           </Collapsible>)
         }}

ee435014323f344286039bcec63218397fcca400
Matt Bruce <mbrucedogs@gmail.com>
2021-11-22 12:56:40 -0600

    refactored more
    Signed-off-by: Matt Bruce <mbrucedogs@gmail.com>

--- src/components/FirebaseReduxHandler.tsx
+++ src/components/FirebaseReduxHandler.tsx
@@ -1,8 +1,6 @@
-import firebase from "firebase"
-import { isEmpty } from "lodash";
 import { useAppDispatch } from '../hooks'
 import { useEffect } from "react";
-import { convertToArray, FirebaseService } from '../services'
+import { FirebaseService } from '../services'
 import {
     favoritesChange,
     historyChange,
@@ -10,14 +8,11 @@ import {
     songsChange,
     songListChange,
     disabledChange,
-    playerStateChange, queueChange, singersChange, settingsChange
+    playerStateChange, 
+    queueChange, 
+    singersChange, 
+    settingsChange
 } from "../store/slices";
-import {
-    QueueItem,
-    Settings,
-    Singer,
-} from "../models/types";
-import { PlayerState, } from "../models"
 
 interface FirebaseReduxHandlerProps {
     isAuthenticated: boolean;
@@ -27,82 +22,21 @@ interface FirebaseReduxHandlerProps {
 export const FirebaseReduxHandler = ({ isAuthenticated, children }: FirebaseReduxHandlerProps) => {
 
     const dispatch = useAppDispatch()
-
     useEffect(() => {
         if (isAuthenticated) {
-            FirebaseService.getSongs().on("value", onSongsChange);
-            FirebaseService.getSongLists().on("value", onSongListChange);
-            FirebaseService.getPlayerSettings().on("value", onSettingsChange);
-            FirebaseService.getPlayerSingers().on("value", onSingersChange);
-            FirebaseService.getPlayerQueue().on("value", onQueueChange);
-            FirebaseService.getPlayerState().on("value", onPlayerStateChange);
-            FirebaseService.getNewSongs().on("value", onLatestSongsChange);
-            FirebaseService.getHistory().on("value", onHistoryChange);
-            FirebaseService.getFavorites().on("value", onFavoritesChange);
-            FirebaseService.getDisabled().on("value", onDisabledChange);
+            FirebaseService.getSongs().on("value", async (snapshot) => { dispatch(songsChange(snapshot))});
+            FirebaseService.getSongLists().on("value", async (snapshot) => { dispatch(songListChange(snapshot))});
+            FirebaseService.getNewSongs().on("value", async (snapshot) => { dispatch(latestSongsChange(snapshot))});
+            FirebaseService.getHistory().on("value", async (snapshot) => { dispatch(historyChange(snapshot))});
+            FirebaseService.getFavorites().on("value", async (snapshot) => { dispatch(favoritesChange(snapshot))});
+            FirebaseService.getDisabled().on("value", async (snapshot) => { dispatch(disabledChange(snapshot))});
+
+            FirebaseService.getPlayerSettings().on("value", async (snapshot) => { dispatch(settingsChange(snapshot))});
+            FirebaseService.getPlayerSingers().on("value", async (snapshot) => { dispatch(singersChange(snapshot))});
+            FirebaseService.getPlayerQueue().on("value", async (snapshot) => { dispatch(queueChange(snapshot))});
+            FirebaseService.getPlayerState().on("value", async (snapshot) => { dispatch(playerStateChange(snapshot))});
         }
-    }, [isAuthenticated])
-
-    ////////////////////////////////
-    //Player 
-    ////////////////////////////////
-    const onPlayerStateChange = async (items: firebase.database.DataSnapshot) => {
-        let state: PlayerState = PlayerState.stopped;
-        let s = items.val();
-        if (!isEmpty(s)) { state = s; }
-        dispatch(playerStateChange(state));
-    };
-
-    const onSingersChange = async (items: firebase.database.DataSnapshot) => {
-        convertToArray<Singer>(items)
-            .then(result => dispatch(singersChange(result)));
-    };
-
-    const onQueueChange = async (items: firebase.database.DataSnapshot) => {
-        convertToArray<QueueItem>(items)
-            .then(result => {
-                let sorted = result.sort((a: QueueItem, b: QueueItem) => {
-                    return a.order - b.order;
-                });
-                dispatch(queueChange(sorted))
-            });
-    };
-
-    const onSettingsChange = async (items: firebase.database.DataSnapshot) => {
-        let s = items.val() as Settings;
-        if(s){
-            dispatch(settingsChange(s));
-        } else { 
-            dispatch(settingsChange({autoadvance:false, userpick: false }))
-        }
-    };
-
-    ////////////////////////////////
-    //Controller 
-    ////////////////////////////////
-    const onLatestSongsChange = async (items: firebase.database.DataSnapshot) => {
-        dispatch(latestSongsChange(items));
-    };
-
-    const onHistoryChange = async (items: firebase.database.DataSnapshot) => {
-       dispatch(historyChange(items));
-    };
-
-    const onFavoritesChange = async (items: firebase.database.DataSnapshot) => {
-        dispatch(favoritesChange(items));
-    };
-
-    const onDisabledChange = async (items: firebase.database.DataSnapshot) => {
-        dispatch(disabledChange(items));
-    };
-
-    const onSongsChange = async (items: firebase.database.DataSnapshot) => {
-        dispatch(songsChange(items));
-    };
-
-    const onSongListChange = async (items: firebase.database.DataSnapshot) => {
-        dispatch(songListChange(items));
-    };
+    }, [isAuthenticated, dispatch])
 
     return <>{children}</>
 }
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,9 +1,11 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import firebase from "firebase"
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
+import { convertToArray } from '../../services'
+import { isEmpty } from "lodash";
 import { 
   QueueItem, 
   Settings, 
   Singer, 
-  Song
 } from '../../models/types';
 import { PlayerState } from '../../models';
 
@@ -11,55 +13,74 @@ interface PlayerSliceHelper {
   playerState: PlayerState;
   singers: Singer[];
   queue: QueueItem[];
-  selectedSong?: Song;
-  selectedSongInfo?: Song;
   settings: Settings;
 }
 
 const initialState: PlayerSliceHelper = {
   queue: [],
   singers: [],
-  selectedSong: undefined,
-  selectedSongInfo: undefined,
   playerState: PlayerState.stopped,
   settings: {autoadvance:false, userpick:false}
 }
 
+export const settingsChange = createAsyncThunk<Settings, firebase.database.DataSnapshot>(
+  'settings/change',
+  async (snapshot: firebase.database.DataSnapshot) => {
+    let settings = snapshot.val() as Settings;
+    if(settings){
+      return settings;
+    } else { 
+        return {autoadvance:false, userpick: false }
+    }
+  }
+)
+
+export const queueChange = createAsyncThunk<QueueItem[], firebase.database.DataSnapshot>(
+  'queue/change',
+  async (snapshot: firebase.database.DataSnapshot) => {
+    let queue = await convertToArray<QueueItem>(snapshot)
+    let sorted = queue.sort((a: QueueItem, b: QueueItem) => {
+      return a.order - b.order;
+    });
+    return sorted;
+  }
+)
+
+export const playerStateChange = createAsyncThunk<PlayerState, firebase.database.DataSnapshot>(
+  'playerState/change',
+  async (snapshot: firebase.database.DataSnapshot) => {
+    let state: PlayerState = PlayerState.stopped;
+    let s = snapshot.val();
+    if (!isEmpty(s)) { state = s; }
+    return state;
+  }
+)
+
+export const singersChange = createAsyncThunk<Singer[], firebase.database.DataSnapshot>(
+  'singers/change',
+  async (snapshot: firebase.database.DataSnapshot) => {
+    return await convertToArray<Singer>(snapshot)
+  }
+)
+
 export const playerSlice = createSlice({
   name: 'player',
   initialState,
-  reducers: {
-    //helper
-    selectedSongChange: (state, action: PayloadAction<{song?: Song}>) => {
-      state.selectedSong = action.payload.song;
-    },
-    selectedSongInfoChange: (state, action: PayloadAction<{song?: Song}>) => {
-      state.selectedSongInfo = action?.payload.song;
-    },
-    //settings
-    settingsChange: (state, action: PayloadAction<Settings>) => {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(settingsChange.fulfilled, (state, action: PayloadAction<Settings>) => {
       state.settings = action.payload;
-    },
-    //queue
-    queueChange: (state, action: PayloadAction<QueueItem[]>) => {
+    })
+    builder.addCase(queueChange.fulfilled, (state, action: PayloadAction<QueueItem[]>) => { 
       state.queue = action.payload;
-    },
-    //state
-    playerStateChange: (state, action: PayloadAction<PlayerState>) => {
+    })  
+    builder.addCase(playerStateChange.fulfilled, (state, action: PayloadAction<PlayerState>) => {
       state.playerState = action.payload;
-    },
-    //singers
-    singersChange: (state, action: PayloadAction<Singer[]>) => {
+    })
+    builder.addCase(singersChange.fulfilled, (state, action: PayloadAction<Singer[]>) => {
       state.singers = action.payload;
-    }
-  }  
+    })
+  }
 })
 
-export const { 
-  settingsChange,
-  selectedSongChange, selectedSongInfoChange,
-  queueChange,  
-  playerStateChange, 
-  singersChange
-  } = playerSlice.actions
 export default playerSlice.reducer
